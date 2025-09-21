327e8833f3c703ac208594b59655609c3f6ce27b
Ameer-ui <ameersmaniyar@gmail.com>
2025-09-21 21:57:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import gdown
 import os
 
-st.title("🧠 Brain Tumor Detection")
+st.title("🧠 Brain Tumor Detection (Binary)")
 
 # Download model from Google Drive if not exists
 model_url = 'https://drive.google.com/uc?id=1pgu7O2OqH-QWxVBjb6KvmNu1F1UbGqKf'  # Replace with your ID
@@ -17,27 +17,3 @@ if not os.path.exists(model_path):
 
 # Load model
 model = load_model(model_path)
-
-# Upload image
-uploaded_file = st.file_uploader("Upload an MRI image", type=["jpg","jpeg","png"])
-
-if uploaded_file is not None:
-    img = Image.open(uploaded_file)
-    st.image(img, caption='Uploaded MRI', use_column_width=True)
-
-    # Preprocess image
-    img = img.resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-
-    # Predict
-    pred = model.predict(img_array)[0][0]
-    if pred > 0.5:
-        result = "Tumor Detected"
-    else:
-        result = "No Tumor"
-
-
-
-
-
